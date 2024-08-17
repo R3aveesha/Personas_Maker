32882644179f8b7976f4fa7ed0fcd3adf166dcb9
raveesha gimhan <146311393+R3aveesha@users.noreply.github.com>
2024-08-17 23:19:39 +0530

    updated

--- Project/src/Components/persona_01.jsx
+++ Project/src/Components/persona_01.jsx
@@ -1,11 +1,8 @@
 import React from 'react';
-import img from '../Components/img.jpg'
+import img from '../Components/img.jpg';
 import '../App.css';
 
-
-
 function Persona_01() {
-    
   return (
     <div className="resume-container">
       <div className="header">
@@ -17,12 +14,12 @@ function Persona_01() {
 
       <div className="about-section">
         <div className="profile-pic">
-          <img src={img} alt="Helene Paquet" />
+          <img src={img} alt="Portrait of Helene Paquet" className="rounded-full w-32 h-32 object-cover" />
         </div>
         
-        <div className="about-text">
-          <h2>About Me</h2>
-          <p class="font-serif">
+        <div className="about-text ml-15">
+          <h2 className="ml-5">About Me</h2>
+          <p className="font-serif ml-5">
             Talented, versatile, and out-of-the-box graphic designer with seven
             years of experience. Expert in using all major graphic design
             platforms and experienced in advanced color theory.
@@ -43,17 +40,16 @@ function Persona_01() {
         <h2>Professional Career</h2>
         <div className="job">
           <h3>Graphic Designer</h3>
-
-          <p style={{padding :8}}>Studio Showde - 2020</p>
+          <p style={{ padding: 8 }}>Studio Showde - 2020</p>
           <ul>
-            <li>Built browser user interfaces with basic knowledge of HTML, CSS, and JAVASCRIPT</li>
-            <li>Developed a friendly-user keyboard and maintained a functional keyboard layout</li>
+            <li>Built browser user interfaces with basic knowledge of HTML, CSS, and JavaScript</li>
+            <li>Developed a user-friendly keyboard and maintained a functional keyboard layout</li>
           </ul>
         </div>
         
         <div className="job">
           <h3>Senior Designer</h3>
-          <p style={{padding:8,}}>Poucek and Lage - 2021</p>
+          <p style={{ padding: 8 }}>Poucek and Lage - 2021</p>
           <ul>
             <li>Created various design work for clients using Adobe Illustrator and Photoshop</li>
             <li>Led and coordinated graphic design projects from conception to completion</li>
